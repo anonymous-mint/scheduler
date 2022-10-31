ace4d3a136643d4fcad86f268c92aca0bdf3aa4c
anonymous-mint <annonymuskmint@gmail.com>
2022-10-31 22:28:30 +0530

    Added typescript config

---
+++ pages/spaces/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+import styles from "../../styles/Home.module.css";
+export default function Home() {
+  const route = useRouter()
+  const handleTileSelection=()=>{
+    route.push('/')
+  }
+  return (
+    <div>
+      <Head>
+        <title>Scheduler</title>
+        <meta name="Scheduler" content="Assign your meeting rooms" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className={styles.tile} onClick={handleTileSelection}>
+          1
+        </div>
+      </main>
+    </div>
+  );
+}
